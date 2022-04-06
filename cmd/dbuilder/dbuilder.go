93df2c0fc33de7c946801cb3ffb2e03dff4dbcfc
Jens Gerke <jens.gerke@direktiv.io>
2022-04-06 16:55:26 +1000

    initial
    Signed-off-by: Jens Gerke <jens.gerke@direktiv.io>

--- cmd/dbuilder/dbuilder.go
+++ cmd/dbuilder/dbuilder.go
@@ -6,6 +6,9 @@ import (
 	dbuild "github.com/jensg-st/dbuilder/cmd/internal/dbuilder"
 
 	"github.com/spf13/cobra"
+
+	_ "github.com/direktiv/direktiv-apps/pkg/reusable"
+	_ "github.com/santhosh-tekuri/jsonschema"
 )
 
 func main() {
